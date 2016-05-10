172bd3761ac0fd6a6614eca35ff888d98e9b32b1
giusepped <giusdesan@gmail.com>
2016-05-10 16:04:51 +0100

    refactored get_data method outside of member class@

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,12 +1,14 @@
+require_relative '../../lib/data_api_call'
+
 class MembersController < ApplicationController
 
 	def index
 	end
 	
 	def fetch
-		member = Member.new
-		member_data = member.get_data(params[:postcode])
-		redirect_to member_path(id: member_data["Members"]["Member"]["Member_Id"])
+		@data_call = DataApiCall.new
+		@member_data = @data_call.get_data(params[:postcode])
+		redirect_to member_path(id: @member_data["Members"]["Member"]["Member_Id"])
 	end
 
 	def show
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,6 +1,3 @@
-require 'net/http'
-require 'crack'
-
 class Member
 
 	attr_reader :name
@@ -11,34 +8,6 @@ class Member
 
 	def set_properties(response)
 		@name = response['Members']['Member']['FullTitle']
-	end
-
-	def get_data(postcode)
-		url = url_builder(postcode)
-		req = request(url)
-		res = response(url, req)
-		convert_xml_to_json(res)
-	end
-
-	private
-
-	def url_builder(postcode)
-		postcode = postcode.tr(" ", "")
-		url = "http://data.parliament.uk/membersdataplatform/services/mnis/members/query/fymp=#{postcode}/"
-		URI.parse(url)
-	end
-
-	def request(uri)
-		Net::HTTP::Get.new(uri)
-	end
-
-	def response(uri, req)
-		res = Net::HTTP.new(uri.host, uri.port)
-		res.request(req).body
-	end
-
-	def convert_xml_to_json(xml)
-		Crack::XML.parse(xml)
 	end
 
 end
