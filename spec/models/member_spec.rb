0be0d4ab92cb1cda31d28dd316dc48bffdfacd51
giusepped <giusdesan@gmail.com>
2016-05-10 16:27:53 +0100

    commented out vacuous test

--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -12,13 +12,13 @@ describe Member, type: :model do
 		end
 	end
 
-	describe '#get_data' do
-		feature 'External request' do
-			it 'queries mnis' do
-			    uri = URI('http://data.parliament.uk/membersdataplatform/services/mnis/members/query/fymp=e29jn/Committees%7CBiographyEntries%7CInterests/')
-			    response = Net::HTTP.get(uri)
-			    expect(response).to be_an_instance_of(String)
-			end
-		end
-	end
+	# describe '#get_data' do
+	# 	feature 'External request' do
+	# 		it 'queries mnis' do
+	# 		    uri = URI('http://data.parliament.uk/membersdataplatform/services/mnis/members/query/fymp=e29jn/Committees%7CBiographyEntries%7CInterests/')
+	# 		    response = Net::HTTP.get(uri)
+	# 		    expect(response).to be_an_instance_of(String)
+	# 		end
+	# 	end
+	# end
   end
